2677a5371478d78c66353e6dc4d7443272650149
NACIMSAAFI <7715@holbertonstudents.com>
2024-01-11 21:51:15 +0100

    Update print prototype

--- binary_trees.h
+++ binary_trees.h
@@ -33,6 +33,6 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(struct binary_tree_s *root);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif
