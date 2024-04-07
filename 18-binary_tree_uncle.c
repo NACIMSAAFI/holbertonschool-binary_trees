64b1dabc59f5c42299040e275c2aef283f86bb0d
NACIMSAAFI <7715@holbertonstudents.com>
2024-04-07 22:59:23 +0100

    Add 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,14 +8,11 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grand;
-
-	if (!node)
+	if (node == NULL ||
+	    node->parent == NULL ||
+	    node->parent->parent == NULL)
 		return (NULL);
-	grand = node->parent->parent;
-	if (node->parent && grand && grand->left == node->parent)
-		return (grand->right);
-	else if (node->parent && grand && grand->right == node->parent)
-		return (grand->left);
-	return (NULL);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
